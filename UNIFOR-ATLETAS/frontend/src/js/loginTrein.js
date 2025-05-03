be3b3b8dfe771420ac156f43beecce926993e5b1
soulzuhn <rhyandarocha2006@gmail.com>
2025-05-03 00:09:49 -0300

    Vite + Bootstrap

--- UNIFOR-ATLETAS/frontend/src/js/loginTrein.js
+++ UNIFOR-ATLETAS/frontend/src/js/loginTrein.js
@@ -5,7 +5,8 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const senha = document.getElementById('password').value;
 
   try {
-    const response = await fetch('https://unifor-atletas.onrender.com/api/usuarios/login', {
+    const response = await fetch("http://localhost:3000/api/usuarios/login", {
+
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
